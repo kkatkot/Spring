385ef61a7baebd0f4b4792a37c549374cb34ce2c
kkatkot <81512097+kkatkot@users.noreply.github.com>
2022-11-03 22:34:49 +0200

    Create MySpringConsoleHelloWorldApplication.java

---
+++ MySpringConsoleHelloWorldApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+
+@SpringBootApplication
+public class MySpringConsoleHelloWorldApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		System.out.println("Begin of main");
+		SpringApplication.run(MySpringConsoleHelloWorldApplication.class, args);
+		System.out.println("End of main");
+	}
+	
+	@Override
+	public void run(String...args) throws Exception {
+		System.out.println("Hello from Spring Boot");
+	}
+
+}
